4be5ce1855d1a4fa0cd2205fa2f65e381e8c38da
Everton Correa <eversc@yahoo.com.br>
2020-04-29 13:32:50 -0300

    reorg + teste CI

--- src/main.go
+++ src/main.go
@@ -8,13 +8,16 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-
+func spendTime() {
 	x := 0.0001
 	for i := 0; i <= 1000000; i++ {
 		x += math.Sqrt(x)
 	}
+}
 
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	spendTime()
 	fmt.Fprintf(w, message.Message())
 }
 
